ceae834d8e4fbc3edd0baf26e3c72c0da6f7c712
Rajdeep9996 <158083423+Rajdeep9996@users.noreply.github.com>
2024-01-30 02:29:10 +0530

    Datatypes

---
+++ Datatypes.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+// String Datatype
+string name;
+
+cout<<"What is your name?"<< endl;
+cin>>name;
+cout<<"Hello! " <<name<< endl;
+
+// Character Datatype
+
+char charValue = 'R';
+cout<<"Stored Character Value is "<<charValue<< endl;
+
+// Integer Datatype
+int intValue = 20;
+cout<<"Stored integer Value is "<<intValue<< endl;
+
+// Floating Point Datatype
+float floatValue = 55.21;
+cout<<"Stored float Value is "<<floatValue<< endl;
+
+// Boolean Datatype
+bool boolValue = false;
+cout<<"Stored Boolean Value is "<<boolValue<< endl;
+
+
+
+return 0;
+}
